8ecb7dc6c806605f33567a5c1558353aca51b7d2
Malinda Kumarasinghe <kumarasinghe@axinom.com>
2018-09-28 09:39:09 +0300

    Added DB data retriavl  methods

--- src/app/db-access/db-access.component.ts
+++ src/app/db-access/db-access.component.ts
@@ -20,6 +20,22 @@ export class DbAccessComponent {
     
   }
 
+  public getDocument(){
+    var docRef = this.afs.firestore.collection("cities").doc("SF");
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+  }
+
+
   public addDocument(){
     this.afs.firestore.collection("autoHub").add({
       first: "Ada",
@@ -32,5 +48,31 @@ export class DbAccessComponent {
   .catch(function(error) {
       console.error("Error adding document: ", error);
   });
+  }
+
+
+  public testAdd(){
+    var citiesRef = this.afs.firestore.collection("cities");
+
+citiesRef.doc("SF").set({
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+citiesRef.doc("LA").set({
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+citiesRef.doc("DC").set({
+    name: "Washington, D.C.", state: null, country: "USA",
+    capital: true, population: 680000,
+    regions: ["east_coast"] });
+citiesRef.doc("TOK").set({
+    name: "Tokyo", state: null, country: "Japan",
+    capital: true, population: 9000000,
+    regions: ["kanto", "honshu"] });
+citiesRef.doc("BJ").set({
+    name: "Beijing", state: null, country: "China",
+    capital: true, population: 21500000,
+    regions: ["jingjinji", "hebei"] });
   }
 }
