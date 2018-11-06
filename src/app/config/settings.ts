d3cbc8e5628d8f982cba16c4be65e62e746a55b2
Malinda <malindakpt@gmail.com>
2018-11-06 08:44:54 +0530

    Preview for model selection in add vehicle

--- src/app/config/settings.ts
+++ src/app/config/settings.ts
@@ -1,8 +1,9 @@
 export class Settings {
   public static LOGIN_TIMEOUT = 3000;
   public static NEWS_FETCH_COUNT = 7;
-  public static VEHICLE_CATEGORIES = [ 'Car', 'Van', 'Bus', '3-wheeler', 'Motor Bike', 'Truck' ];
-  public static VEHICLE_BRANDS = ['Nissan', 'Toyota', 'Isuzu', 'Suzuki'];
+  public static VEHICLE_CATEGORIES = [ 'Van', 'Bus', '3-wheeler', 'Jeep', 'Double Cab', 'Motor Bike',
+    'Truck', 'Car-Sedan', 'Car-Hatchback', 'Car-Wagon'];
+  public static VEHICLE_BRANDS = ['NISSAN', 'HONDA', 'TOYOTA', 'ISUZU', 'SUZUKI'];
   public static COUNTRIES = ['Japan', 'India', 'United Kindom', 'China'];
   public static FUEL_TYPES = ['Petrol', 'Hybrid', 'Disel', 'Electric'];
   public static VALIDATE_ADD_VEHICLE = true;
