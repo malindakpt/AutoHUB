f70dd47df7fd9b595874cdd7f62312628a314051
Malinda <malindakpt@gmail.com>
2018-12-13 14:27:01 +0530

    Fixed minor issues

---
+++ src/app/components-sub/sell-details/sell-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sell-details',
+  templateUrl: './sell-details.component.html',
+  styleUrls: ['./sell-details.component.scss']
+})
+export class SellDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
